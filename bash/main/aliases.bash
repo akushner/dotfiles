8951ab48e67d49d663d1292bad17af2db107ea80
Aaron Kushner <akushner@fb.com>
2011-04-05 15:27:39 -0700

    - put in the color ls aliases I like

--- bash/main/aliases.bash
+++ bash/main/aliases.bash
@@ -15,6 +15,13 @@ alias ZZ='suspend'
 alias j='jobs -l'
 alias rehash='hash -r'
 alias unexport='export -n'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
 
 source_local_bash_init_file aliases
 
